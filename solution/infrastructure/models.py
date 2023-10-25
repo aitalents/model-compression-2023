fb9dc2e5640a2623a84c6574a839f7c858d136f8
Arseniy Plyusnin <56270907+arsenplus@users.noreply.github.com>
2023-10-25 18:25:03 +0300

    Added Optimum onnx optimizations

--- solution/infrastructure/models.py
+++ solution/infrastructure/models.py
@@ -1,10 +1,11 @@
+import torch
+from optimum.onnxruntime import ORTModelForSequenceClassification
+from transformers import AutoTokenizer
+from optimum.onnxruntime import ORTQuantizer, OptimizationConfig, ORTOptimizer
+from optimum.onnxruntime.configuration import AutoQuantizationConfig
 from abc import ABC, abstractmethod
-from collections.abc import Callable
 from dataclasses import dataclass
 from typing import List
-
-import torch
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
 
 
 @dataclass
@@ -36,8 +37,44 @@ class TransformerTextClassificationModel(BaseTextClassificationModel):
 
     def _load_model(self):
         self.tokenizer = AutoTokenizer.from_pretrained(self.tokenizer)
-        self.model = AutoModelForSequenceClassification.from_pretrained(self.model_path)
+        self.model = self._optimize_model()
         self.model = self.model.to(self.device)
+
+    def _optimize_model(self):
+        ort_model = ORTModelForSequenceClassification.from_pretrained(
+            self.model_path,
+            export=True,
+            provider=['CUDEExecutionProvider']
+        )
+
+        save_dir = "./optimized_model"
+        quantizer = ORTQuantizer.from_pretrained(ort_model)
+        dqconfig = AutoQuantizationConfig.avx512_vnni(is_static=False, per_channel=False)
+
+        model_quantized_path = quantizer.quantize(
+            save_dir=save_dir,
+            quantization_config=dqconfig
+        )
+
+        ort_model = ORTModelForSequenceClassification.from_pretrained(model_quantized_path)
+        optimizer = ORTOptimizer.from_pretrained(ort_model)
+
+        optimization_config = OptimizationConfig(
+            optimization_level=99,
+            enable_transformers_specific_optimizations=True,
+            optimize_for_gpu=True
+        )
+
+        model_optimized_path = optimizer.optimize(
+            save_dir=save_dir,
+            optimization_config=optimization_config
+        )
+
+        return ORTModelForSequenceClassification.from_pretrained(
+            model_optimized_path,
+            file_name='model_quantized_optimized.onnx'
+        )
+
 
     def tokenize_texts(self, texts: List[str]):
         inputs = self.tokenizer.batch_encode_plus(
@@ -70,4 +107,3 @@ class TransformerTextClassificationModel(BaseTextClassificationModel):
         predictions = self._results_from_logits(logits)
         predictions = [TextClassificationModelData(self.name, **prediction) for prediction in predictions]
         return predictions
-
