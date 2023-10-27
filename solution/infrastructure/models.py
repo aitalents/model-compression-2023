e4a1cc7465ed7be95db6e61cf86fc2e677597472
Anton Shiryaev <med.phisiker@gmail.com>
2023-10-27 04:14:51 +0000

    feat(core): Сделать docker-образы для CPU и GPU
    Сделать docker-образы для CPU и GPU

--- solution/infrastructure/models.py
+++ solution/infrastructure/models.py
@@ -1,3 +1,4 @@
+import os
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from typing import List
@@ -22,7 +23,6 @@ class TextClassificationModelData:
 
 
 class BaseTextClassificationModel(ABC):
-
     def __init__(self, name: str, model_path: str, tokenizer: str):
         self.name = name
         self.model_path = model_path
@@ -40,34 +40,47 @@ class BaseTextClassificationModel(ABC):
 
 
 class TransformerTextClassificationModel(BaseTextClassificationModel):
-
     def _load_model(self):
         sentiment_task = pipeline(
-                "sentiment-analysis",
-                model=self.model_path,
-                tokenizer=self.model_path,
-                device=self.device
-                )
+            "sentiment-analysis",
+            model=self.model_path,
+            tokenizer=self.model_path,
+            device=self.device,
+        )
         return sentiment_task
 
     def __call__(self, input_texts: List[str]) -> List[TextClassificationModelData]:
         predictions = self.model(input_texts, batch_size=len(input_texts))
-        predictions = [TextClassificationModelData(self.name, **prediction) for prediction in predictions]
+        predictions = [
+            TextClassificationModelData(self.name, **prediction)
+            for prediction in predictions
+        ]
         return predictions
 
 
 class OnnxTransformerTextClassificationModel(TransformerTextClassificationModel):
-
     def _load_model(self):
+        device = os.getenv("DEVICE")
+        print(f"Запускаем модель на {device}")
+        
+        if device == "gpu":
+            provider = "CUDAExecutionProvider"
+            infer_device = "cuda:0"
+        else:
+            provider = "CPUExecutionProvider"
+            infer_device = "cpu"
+
         tokenizer = AutoTokenizer.from_pretrained(self.tokenizer)
         model = ORTModelForSequenceClassification.from_pretrained(
-                self.model_path,
-                export=True,
-                provider="CUDAExecutionProvider",
-                session_options=session_options
+            self.model_path,
+            export=True,
+            provider=provider,
+            session_options=session_options,
         )
 
         model_optimizer = OnnxModelOptimizer(model)
         model = model_optimizer.graph_optimization(self.name, model)
 
-        return pipeline("text-classification", model=model, tokenizer=tokenizer, device="cuda:0")
+        return pipeline(
+            "text-classification", model=model, tokenizer=tokenizer, device=infer_device
+        )
